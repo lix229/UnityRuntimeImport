41a91ae21eb2ebc8d5732c0838baeefe73f8bdcb
lix229 <hydronium0712@gmail.com>
2021-02-07 16:40:31 -0500

    Updating functions for toggle Animation playback

---
+++ AnimationEditor/Animation Editor_files/index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+	Particles.init({
+	  selector: '.background',
+	  connectParticles: true,
+	  maxParticals: 150,
+	  color: "#EEEAE9"
+	});
+};
--- AnimationEditor/js/index.js
+++ AnimationEditor/js/index.js
@@ -6,3 +6,14 @@ window.onload = function() {
 	  color: "#EEEAE9"
 	});
 };
+
+
+var toggleAnimation = function(){
+	var animObj = document.getElementsByClassName('cube')[0];
+	if (animObj.style.animationPlayState === 'paused') {
+		animObj.style.animationPlayState = 'running';
+	}
+	else {
+		animObj.style.animationPlayState = 'paused';
+	}
+}
