25fa20bef7f62a958cffa584531bcbd9e893ea7c
lix229 <hydronium0712@gmail.com>
2021-02-09 17:48:14 -0500

    Now catching unfilled input textboxes

--- AnimationEditor/js/index.js
+++ AnimationEditor/js/index.js
@@ -7,10 +7,22 @@ window.onload = function() {
 	});
 };
 
+var
+      animSheet = document.styleSheets[1] // replace 0 with the number of the animSheet that you want to modify
+    , rules = animSheet.cssRules
+    , i = rules.length
+    , ruleItems
+    , keyframe
+	, currentFrameTime = 0
+	, maxFrameTime = 0
+	, keyframeCount = 1
+	, keyFrameList = [["{transform: rotate3d(0, 0, 0, 0);}"], ["{transform: scale3d(1, 1, 1) rotate3D(10, 20, 1, 70deg);}"]]
+	, keyFrameTimes = [0,10000]
+;
 
 var toggleAnimation = function(){
 	var animObj = document.getElementsByClassName('cube')[0];
-	// console.log(animObj.style.animationPlayState);
+	animObj.style.animationDuration = keyFrameTimes[keyFrameTimes.length-1] + 'ms';
 	if (animObj.style.animationPlayState === 'paused') {
 		animObj.style.animationPlayState = 'running';
 		document.getElementById('playStatus').innerHTML = 'PAUSE'
@@ -21,18 +33,7 @@ var toggleAnimation = function(){
 	}
 }
 
-var
-      stylesheet = document.styleSheets[0] // replace 0 with the number of the stylesheet that you want to modify
-    , rules = stylesheet.rules
-    , i = rules.length
-    , ruleItems
-    , keyframe
-	, currentFrameTime = 0
-	, maxFrameTime = 0
-	, keyframeCount = 1
-	, keyFrameList = [["transform: rotate3d(0, 0, 0, 0);"]]
-	, keyFrameTimes = [0]
-;
+
 
 // while (i--) {
 //     ruleItems = rules.item(i);
@@ -42,7 +43,7 @@ var
 //                ruleItems.type === ruleItems.KEYFRAMES_RULE
 //             || ruleItems.type === ruleItems.WEBKIT_KEYFRAMES_RULE
 //         )
-//         && ruleItems.name === "rotate"
+//         && ruleItems.name === "animBody"
 //     ) {
 //         rules = ruleItems.cssRules;
 // 		console.log(rules);
@@ -66,14 +67,40 @@ var
 //     }
 // }
 
+    ruleItems = rules.item(i);
+	console.log(animSheet)
+	animSheet.removeRule(0);
+	var newAnim = "@keyframes animBody { "
+		+ "0%" + keyFrameList[0] +
+		"100%" + keyFrameList[1] +
+	  "}";
+	animSheet.insertRule(newAnim, 0)
+
 
 var submitFrameData = function() {
-	currentFrameTime = document.getElementById('ft').value;
-	console.log(currentFrameTime)
-	if (!keyFrameTimes.includes(currentFrameTime) && typeof(currentFrameTime) != typeof("")) {
-		keyFrameTimes.push(currentFrameTime);
-		keyFrameTimes.sort((a, b) => a - b);
-	}
-	console.log(keyFrameTimes)
+	var 
+	  frameData = fetchNewFrameData()
+	, currentFrameTime = frameData[0]
+	, rotationData = frameData[1]
+	, scaleData = frameData[2]
+	, translateData = frameData[3]
 	
+	if (currentFrameTime.includes("") || rotationData.includes("") || scaleData.includes("") || translateData.includes("")) {
+		alert("Unfilled fields detected.")
+	}
+	else if (keyFrameTimes.includes(currentFrameTime)){
+		//TODO modify existing frame
+	}
+	else {
+		
+	}
+	
+}
+
+var fetchNewFrameData = function() {
+	var currentFrameTime = [document.getElementById('ft').value];
+	var rotationData = [document.getElementById('rx').value, document.getElementById('ry'), document.getElementById('rz'), document.getElementById('rr')];
+	var scaleData = [document.getElementById('sx').value, document.getElementById('sy'), document.getElementById('sz')];
+	var translateData = [document.getElementById('tlx').value, document.getElementById('tly'), document.getElementById('tlz')];
+	return [currentFrameTime, rotationData, scaleData, translateData]
 }
