0af37bd19f5d2ea5fc962afae0017d6cdd63c00a
Kevin Zhou <zhg163@gmail.com>
2021-02-21 07:25:04 +0800

    Added slider to the interface and a timer for counting the animation frametime

--- AnimationEditor/js/index.js
+++ AnimationEditor/js/index.js
@@ -49,10 +49,12 @@ var toggleAnimation = function(){
 	var animObj = document.getElementsByClassName('cube')[0];
 	if (animObj.style.animationPlayState !== 'running') { // Not the best, but set condition to == paused will only work from second click for some reason.
 		animObj.style.animationPlayState = 'running';
+		timer.start();
 		document.getElementById('playStatus').innerHTML = 'PAUSE'
 	}
 	else{
 		animObj.style.animationPlayState = 'paused';
+		timer.stop();
 		document.getElementById('playStatus').innerHTML = 'PLAY'
 	}
 }
@@ -289,3 +291,64 @@ function updateAnimation() {
 // 		console.log(keyFrameList[indexToToggle-1])
 // 	}
 // }
+
+// A timer for displaying the playback time with start, stop, reset and getTime methods.
+
+class Timer {
+	constructor () {
+	  this.isRunning = false;
+	  this.startTime = 0;
+	  this.overallTime = 0;
+	  }
+  
+	_getTimeElapsedSinceLastStart () {
+	  if (!this.startTime) {
+		return 0;
+	  }
+	
+	  return Date.now() - this.startTime;
+	  }
+  
+	start () {
+	  if (this.isRunning) {
+		return console.error('Timer is already running');
+	  }
+  
+	  this.isRunning = true;
+  
+	  this.startTime = Date.now();
+	  }
+  
+	stop () {
+	  if (!this.isRunning) {
+		return console.error('Timer is already stopped');
+	  }
+  
+	  this.isRunning = false;
+  
+	  this.overallTime = this.overallTime + this._getTimeElapsedSinceLastStart();
+	  }
+  
+	reset () {
+	  this.overallTime = 0;
+  
+	  if (this.isRunning) {
+		this.startTime = Date.now();
+		return;
+	  }
+  
+	  this.startTime = 0;
+	  }
+  
+	getTime () {
+	  if (!this.startTime) {
+		return 0;
+	  }
+  
+	  if (this.isRunning) {
+		return this.overallTime + this._getTimeElapsedSinceLastStart();
+	  }
+  
+	  return this.overallTime;
+	  }
+  }
